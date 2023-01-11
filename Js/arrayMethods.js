f4f870572653b8cb01c021c28d4c5aeffb0b7b20
hey-sid <madthasadish@gmail.com>
2023-01-11 22:32:00 +0530

    concat method and join method

--- Js/arrayMethods.js
+++ Js/arrayMethods.js
@@ -62,3 +62,41 @@ document.write("6). " + a + "<br>"); /// Avi, 25, 45, Sid // Initial Output
 
 a.unshift(37);
 document.write("6). " + a + "<br><br>"); /// 37, Avi, 25, 45, Sid // It adds the value from the start
+
+
+
+// Array concat() Method
+//====================
+//eg:1
+
+var a=["Avi", 25, 45, "Sid"];
+
+document.write("7). " + a + "<br>"); /// Avi, 25, 45, Sid // Initial Output
+
+var b= a.concat(37);
+document.write("7). " + b + "<br><br>"); /// Avi, 25, 45, Sid, 37 // It merge the new variable value at the last
+
+
+//eg:2
+
+var a=["Avi", 25, 45, "Sid"];
+var b=["Manish", 55];
+var c=["Supriya", 75];
+
+document.write("8). " + a + "<br>");/// Avi, 25, 45, Sid // Initial Output
+document.write("8). " + b + "<br>");/// Manish, 55 // Initial Output
+document.write("8). " + c + "<br>");/// Supriya, 75 // Initial Output
+var d= a.concat(b,c);
+document.write("8). " + d + "<br><br>"); /// Avi, 25, 45, Sid, Manish, 55, Supriya, 75 // It merge the new variable value at the last
+
+
+// Array join() Method
+//====================
+//eg:1
+
+var a=["Avi", 25, 45, "Sid"];
+
+document.write("9). " + a + "<br>"); /// Avi, 25, 45, Sid // Initial Output
+
+var b= a.join(" ");
+document.write("9). " + b + "<br><br>"); /// Avi 25 45 Sid // It makes single value 
